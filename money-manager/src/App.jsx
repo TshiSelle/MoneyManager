fb76a078f17f88a3ccc56e9aede15d669ce1caa9
OmarHoussami <omar.houssami20@gmail.com>
2021-08-03 21:56:35 +0300

    text edits

--- money-manager/src/App.jsx
+++ money-manager/src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
-  return <div className='App'>sell</div>;
+  return <div className='App'></div>;
 }
 
 export default App;
